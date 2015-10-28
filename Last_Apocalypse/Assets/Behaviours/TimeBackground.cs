7e28ace9e48bc40f6332daff1160f71601a9e0ce
Dunnings <mrdaviddunnings@gmail.com>
2015-10-28 16:06:51 +0000

    UI Work

---
+++ Last_Apocalypse/Assets/Behaviours/TimeBackground.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TimeBackground : MonoBehaviour {
+    public Image m_Image;
+    public Text m_Text;
+    public Color m_colMidnight;
+    public Color m_colMidday;
+    public float m_time = 0f;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_time += Time.deltaTime * 0.3f;
+        if(m_time > 2f) { m_time -= 2f; }
+        float val = m_time;
+        if(val > 1f)
+        {
+            val -= 1f;
+            val = 1f - val;
+        }
+        m_Image.color = Color.Lerp(m_colMidnight, m_colMidday, val);
+        //Color c = (new Color(1f, 1f, 1f, 1f)) - m_Image.color;
+        //c.a = 1f;
+        //m_Text.color = c;
+	}
+}
---
+++ Last_Apocalypse/Assets/Behaviours/TimeDisplay.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class TimeDisplay : MonoBehaviour {
+
+    public Text m_text;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_text.text = DateTime.Now.Hour.ToString("00") + ":" + DateTime.Now.Minute.ToString("00") + ":" + DateTime.Now.Second.ToString("00");
+	}
+}
