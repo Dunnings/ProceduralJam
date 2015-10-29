d3fc93d20a47e8b7f111320b2287c4aedc697b72
OWJBourne <oliverbourne93@gmail.com>
2015-10-29 14:43:01 +0000

    Basic click to move
    Click a point on the screen and it moves a game object to the point
    clicked. next step, moving towards the point.

---
+++ Last_Apocalypse/Assets/Behaviours/CharMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0;
+            gameObject.transform.position = mousePos;
+        }
+	}
+}
