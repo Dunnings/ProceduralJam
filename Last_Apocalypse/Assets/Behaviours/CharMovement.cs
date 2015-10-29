428f454032ecc5d83f553ef0d190808208f5ae40
OWJBourne <oliverbourne93@gmail.com>
2015-10-29 16:13:34 +0000

    Rotacione!
    Botched a rotation as look at function breaks in 2d. Also limited
    didstance to stop bug when sprite (character) reaches mouse point. (far
    from perfect, still with basic sprite)

--- Last_Apocalypse/Assets/Behaviours/CharMovement.cs
+++ Last_Apocalypse/Assets/Behaviours/CharMovement.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class CharMovement : MonoBehaviour {
 
     public float speed = 50.0f;
+    float distance;
 	// Use this for initialization
 	void Start () {
 	
@@ -11,11 +12,21 @@ public class CharMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        // When mouse button is held down
 	    if (Input.GetMouseButton(0))
         {
+            // Get mouse position on screen
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             mousePos.z = 0;
-            //transform.Rotate(mousePos * Time.deltaTime);
+            distance = Vector3.Distance(gameObject.transform.position, mousePos);
+            if (distance > 1)
+            {
+                var pos = Camera.main.WorldToScreenPoint(transform.position);
+                var dir = Input.mousePosition - pos;
+                var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            }
+           
             gameObject.transform.position = Vector3.Lerp(gameObject.transform.position,
                                              mousePos,
                                              1 / (speed * (Vector3.Distance(gameObject.transform.position, mousePos))));
