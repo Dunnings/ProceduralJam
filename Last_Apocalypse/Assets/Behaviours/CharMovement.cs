67acb713d4c8a550a29f6fc26d1d3f92b8042347
OWJBourne <oliverbourne93@gmail.com>
2015-10-29 15:23:57 +0000

    Character move to mouse
    Now lerps to mousepoint for smooth following of mouse position not
    direct transform to position.

--- Last_Apocalypse/Assets/Behaviours/CharMovement.cs
+++ Last_Apocalypse/Assets/Behaviours/CharMovement.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class CharMovement : MonoBehaviour {
 
+    public float speed = 50.0f;
 	// Use this for initialization
 	void Start () {
 	
@@ -10,11 +11,14 @@ public class CharMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	    if (Input.GetMouseButtonDown(0))
+	    if (Input.GetMouseButton(0))
         {
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             mousePos.z = 0;
-            gameObject.transform.position = mousePos;
+            //transform.Rotate(mousePos * Time.deltaTime);
+            gameObject.transform.position = Vector3.Lerp(gameObject.transform.position,
+                                             mousePos,
+                                             1 / (speed * (Vector3.Distance(gameObject.transform.position, mousePos))));
         }
 	}
 }
