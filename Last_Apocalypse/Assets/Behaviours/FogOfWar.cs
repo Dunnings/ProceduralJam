d0de186a950f82474356ec5aff1971f1f6a88fc4
Dan Weston <westondaniel93@gmail.com>
2015-10-29 10:09:39 +0000

    Black fog clears with player pos and with a sight radius!

--- Last_Apocalypse/Assets/Behaviours/FogOfWar.cs
+++ Last_Apocalypse/Assets/Behaviours/FogOfWar.cs
@@ -4,20 +4,20 @@ using System.Collections;
 //ensure that GameObject has SpriteRenderer for fog
 public class FogOfWar : MonoBehaviour 
 {
-    private GameObject undisFogGO;
-    private GameObject[,] blackFog;
-    private GameObject[,] greyFog;
-    private GameObject fogGO;
+    private GameObject _blackFog;
+    private GameObject _alphaFog;
 
-    private SpriteRenderer _undiscoveredFog;
+    private GameObject[,] blackFog;
+    private GameObject[,] alphaFog;
+    
+
 
     //Player Ref
     public GameObject player;  
 
     //Level width and height
-    public int mapWidth, mapHeight;
-
-    public int tileWidth, tileHeight;
+    public int mapWidth, mapHeight,
+        tileWidth, tileHeight, sightRadius;
 
     /// <summary>
     /// Set up components for depth fog
@@ -25,21 +25,18 @@ public class FogOfWar : MonoBehaviour
     void Awake()
     {
         blackFog = new GameObject[mapWidth, mapHeight];
-        greyFog = new GameObject[mapWidth, mapHeight];
+        alphaFog = new GameObject[mapWidth, mapHeight];
         //set up both GameObjects for Fog  
-        undisFogGO = new GameObject ("Black Fog");
-        undisFogGO.AddComponent<SpriteRenderer>();
+        _blackFog = new GameObject ("Black Fog");
+        _alphaFog = new GameObject("Alpha Fog");
+        _blackFog.AddComponent<SpriteRenderer>();
+        _alphaFog.AddComponent<SpriteRenderer>();
 
-        CreateFog(Color.black, blackFog, undisFogGO);
+        CreateFog(Color.black, blackFog, _blackFog);        
+        CreateFog(new Color(0.0f, 0.0f, 0.0f, 0.5f), alphaFog, _alphaFog);
 
-        //fogGO = new GameObject("Alpha Fog");
-        //CreateFog(new Color(0.0f,0.0f,0.0f,0.5f), greyFog, fogGO);
-        //fogGO.transform.parent = this.transform;
-
-        
-
-        undisFogGO.transform.parent = this.transform;
-        //fogGO.transform.parent = this.transform;    
+        _alphaFog.transform.parent = this.transform;
+        _blackFog.transform.parent = this.transform;
     }
 
     /// <summary>
@@ -79,17 +76,61 @@ public class FogOfWar : MonoBehaviour
 
     void Update()
     {
+
+        //get array pos from player pos
         float posX = player.transform.position.x * 100.0f / tileWidth;
         float posY = player.transform.position.y * 100.0f / tileHeight;
 
-        //if (posX % tileWidth == 0.0f ||
-        //    posY % tileHeight == 0.0f)
+        //if (posX % sightRadius == 0 ||
+        //    posY % sightRadius == 0)
         //{
-            if (blackFog[(int)posX, (int)posY].activeSelf == true)
-            {
-                blackFog[(int)posX, (int)posY].SetActive(false);
-            }
+            ClearFog((int)posX, (int)posY, blackFog);
         //}
+
+        ////set current player pos fog tiles active false
+        //if (blackFog[(int)posX, (int)posY].activeSelf == true)
+        //{
+        //    blackFog[(int)posX, (int)posY].SetActive(false);
+        //}
+       
+    }
+
+    void ClearFog(int _x, int _y, GameObject[,] fogTiles)
+    {
+        // from the player point - the sight radius up to the player 
+        //point + the sight radius
+        for (int x = _x - sightRadius; x < _x + sightRadius; x++)
+        {
+            for (int y = _y-sightRadius; y < _y + sightRadius; y++)
+            {
+                //make sure X does not go out of range of map
+                if(x < 0.0f)
+                {
+                    x = 0;
+                }
+                if(x > mapWidth)
+                {
+                    x = mapWidth;
+                }
+
+                //make sure Y does not go out of range of map
+                if (y < 0.0f)
+                {
+                    y = 0;
+                }
+                if (y > mapHeight)
+                {
+                    y = mapHeight;
+                }
+
+                //if the targted tile is active
+                if (fogTiles[x, y].activeSelf)
+                {
+                    //set to be inactive
+                    fogTiles[x, y].SetActive(false);
+                }
+            }            
+        }
     }
   
 }
