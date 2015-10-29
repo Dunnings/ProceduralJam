10ad00da05c05c2571869a48482e53a8c585e78c
Dunnings <mrdaviddunnings@gmail.com>
2015-10-29 17:16:10 +0000

    Added following Camera. Tweaked character movement

--- Last_Apocalypse/Assets/Behaviours/CharMovement.cs
+++ Last_Apocalypse/Assets/Behaviours/CharMovement.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class CharMovement : MonoBehaviour {
 
+    public Animator anim;
     public float speed = 50.0f;
     float distance;
 	// Use this for initialization
@@ -19,17 +20,26 @@ public class CharMovement : MonoBehaviour {
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             mousePos.z = 0;
             distance = Vector3.Distance(gameObject.transform.position, mousePos);
-            if (distance > 1)
+            if (distance > 0.01f)
             {
                 var pos = Camera.main.WorldToScreenPoint(transform.position);
                 var dir = Input.mousePosition - pos;
                 var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
                 transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+                Debug.Log(angle);
+                anim.SetBool("isWalking", true);
             }
-           
+            else
+            {
+                anim.SetBool("isWalking", false);
+            }
             gameObject.transform.position = Vector3.Lerp(gameObject.transform.position,
                                              mousePos,
                                              1 / (speed * (Vector3.Distance(gameObject.transform.position, mousePos))));
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
         }
 	}
 }
---
+++ Last_Apocalypse/Assets/GameCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameCamera : MonoBehaviour {
+
+    public GameObject m_target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.Lerp(transform.position, new Vector3(m_target.transform.position.x, m_target.transform.position.y, transform.position.z), Time.deltaTime * (1f + (Vector3.Distance(m_target.transform.position, transform.position) * 2f)));
+	}
+}
