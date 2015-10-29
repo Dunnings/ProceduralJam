b08dadcb2c47f79401d155d7b05e420f1b9d1d14
Dan Weston <westondaniel93@gmail.com>
2015-10-29 12:16:42 +0000

    Y'all asked for a circle FoW y'all get FoW - Also I made a grass tile :3 (testing not sure if it's actually needed)

--- Last_Apocalypse/Assets/Behaviours/FogOfWar.cs
+++ Last_Apocalypse/Assets/Behaviours/FogOfWar.cs
@@ -17,7 +17,7 @@ public class FogOfWar : MonoBehaviour
 
     //Level width and height
     public int mapWidth, mapHeight,
-        tileWidth, tileHeight, sightRadius;
+        tileWidth, tileHeight, sightRadius, sightBuffer;
 
     /// <summary>
     /// Set up components for depth fog
@@ -29,14 +29,19 @@ public class FogOfWar : MonoBehaviour
         //set up both GameObjects for Fog  
         _blackFog = new GameObject ("Black Fog");
         _alphaFog = new GameObject("Alpha Fog");
-        _blackFog.AddComponent<SpriteRenderer>();
-        _alphaFog.AddComponent<SpriteRenderer>();
+        //SpriteRenderer bF = _blackFog.AddComponent<SpriteRenderer>();
+        //SpriteRenderer af = _alphaFog.AddComponent<SpriteRenderer>();        
 
         CreateFog(Color.black, blackFog, _blackFog);        
-        CreateFog(new Color(0.0f, 0.0f, 0.0f, 0.5f), alphaFog, _alphaFog);
+        CreateFog(new Color(0.0f, 0.0f, 0.0f, 0.75f), alphaFog, _alphaFog);
 
         _alphaFog.transform.parent = this.transform;
         _blackFog.transform.parent = this.transform;
+    }
+
+    void InitFog(float _mapWidth, float _mapHeight, float _tileWidth, float _tileHeight)
+    {
+
     }
 
     /// <summary>
@@ -48,16 +53,16 @@ public class FogOfWar : MonoBehaviour
     {
         //temp holder for fog text/sprite
         Texture2D fog = new Texture2D(tileWidth, tileHeight);
-        for (int x = 0; x < mapWidth; x++)
+        for (int x = 0; x < tileWidth; x++)
         {
-            for (int y = 0; y < mapWidth; y++)
+            for (int y = 0; y < tileHeight; y++)
             {
                 fog.SetPixel(x, y, fogCol);
             }
         }
         fog.Apply();
 
-        Sprite _fogSprite = Sprite.Create(fog, new Rect(0.0f, 0.0f, fog.width, fog.height), new Vector2(0.0f, 0.0f));
+        Sprite _fogSprite = Sprite.Create(fog, new Rect(0.0f, 0.0f, fog.width, fog.height), new Vector2(0.5f, 0.5f));
        
 
         //loop over texture width and height
@@ -70,10 +75,14 @@ public class FogOfWar : MonoBehaviour
                 fogTiles[x, y].transform.position = new Vector3(x * (tileWidth / 100.0f), y * (tileHeight / 100.0f), 0.0f);
                 SpriteRenderer f = fogTiles[x, y].AddComponent<SpriteRenderer>();
                 f.sprite = _fogSprite;
+                f.sortingOrder = 32767;
             }
         }
     }
 
+    /// <summary>
+    /// Called every frame, checks player pos and updates the fog
+    /// </summary>
     void Update()
     {
 
@@ -81,63 +90,120 @@ public class FogOfWar : MonoBehaviour
         float posX = player.transform.position.x * 100.0f / tileWidth;
         float posY = player.transform.position.y * 100.0f / tileHeight;
 
-        if (posX >= pastPosX + sightRadius || posX <= pastPosX - sightRadius ||
-            posY >= pastPosY + sightRadius || posY <= pastPosY - sightRadius)
+        //if the player has moved into the sight buffer update FoW
+        if (posX >= pastPosX + sightBuffer || posX <= pastPosX - sightBuffer ||
+            posY >= pastPosY + sightBuffer || posY <= pastPosY - sightBuffer)
         {
-            ClearFog((int)pastPosX, (int)pastPosY, alphaFog, true);
+            
             ClearFog((int)posX, (int)posY, blackFog, false);
+            ClearFog((int)pastPosX, (int)pastPosY, alphaFog, true);
             ClearFog((int)posX, (int)posY, alphaFog, false);            
 
+            //reset pastPosition
             pastPosX = posX;
             pastPosY = posY;
-        }
-
-        
-
-        ////set current player pos fog tiles active false
-        //if (blackFog[(int)posX, (int)posY].activeSelf == true)
-        //{
-        //    blackFog[(int)posX, (int)posY].SetActive(false);
-        //}
-       
+        }        
     }
 
+    /// <summary>
+    /// Using the x and y pos and the fogTiles passed
+    /// a circle is created around the point of x and y
+    /// and the fogTiles at the points of the circle created 
+    /// are set to the visibilty defined by the visibility parameter
+    /// </summary>
+    /// <param name="_x"></param>
+    /// <param name="_y"></param>
+    /// <param name="fogTiles"></param>
+    /// <param name="visibility"></param>
     void ClearFog(int _x, int _y, GameObject[,] fogTiles, bool visibility)
     {
-        // from the player point - the sight radius up to the player 
-        //point + the sight radius
-        for (int x = _x - sightRadius; x < _x + sightRadius; x++)
+        //iterate down the line of sight
+        for (int rad = sightRadius; rad >= 0; rad--)
         {
-            for (int y = _y-sightRadius; y < _y + sightRadius; y++)
+            //create a circle with 360 points
+            for (int pointNum = 0; pointNum <= 360; pointNum++)
             {
+                float i = (pointNum * 1.0f) / 360;
+
+                //Get angle for current step
+                var angle = i * Mathf.PI * 2;
+                // the X &amp; Y position for this angle are calculated using Sin &amp; Cos
+                float x = Mathf.Sin(angle) * rad;
+                float y = Mathf.Cos(angle) * rad;
+                Vector3 pos = new Vector3(x, y, 0) + new Vector3(_x,_y,0.0f);
+
                 //make sure X does not go out of range of map
-                if(x < 0.0f)
+                if (pos.x < 0.0f)
                 {
-                    x = 0;
+                    pos.x = 0;
                 }
-                if(x > mapWidth)
+                if (pos.x > mapWidth-1)
                 {
-                    x = mapWidth;
+                    pos.x = mapWidth;
                 }
 
                 //make sure Y does not go out of range of map
-                if (y < 0.0f)
+                if (pos.y < 0.0f)
                 {
-                    y = 0;
+                    pos.y = 0;
                 }
-                if (y > mapHeight)
+                if (pos.y > mapHeight-1)
                 {
-                    y = mapHeight;
+                    pos.y = mapHeight;
                 }
 
-                //if the targted tile is active
-                if (fogTiles[x, y].activeSelf != visibility)
+                if (fogTiles[(int)pos.x, (int)pos.y].activeSelf != visibility)
                 {
                     //set to be inactive
-                    fogTiles[x, y].SetActive(visibility);
+                    fogTiles[(int)pos.x, (int)pos.y].SetActive(visibility);
                 }
-            }            
+
+            }
+
         }
+
+        //// old creates a square
+        //from the player point - the sight radius up to the player 
+        ////point + the sight radius
+        //for (int x = _x - sightRadius; x < _x + sightRadius; x++)
+        //{
+        //    for (int y = _y-sightRadius; y < _y + sightRadius; y++)
+        //    {
+        //        //make sure X does not go out of range of map
+        //        if(x < 0.0f)
+        //        {
+        //            x = 0;
+        //        }
+        //        if(x > mapWidth)
+        //        {
+        //            x = mapWidth;
+        //        }
+
+        //        //make sure Y does not go out of range of map
+        //        if (y < 0.0f)
+        //        {
+        //            y = 0;
+        //        }
+        //        if (y > mapHeight)
+        //        {
+        //            y = mapHeight;
+        //        }
+
+        //        float ang = 360;
+        //        Vector3 pos;
+        //        pos.x = x + sightRadius * Mathf.Sin(ang * Mathf.Deg2Rad);
+        //        pos.y = y + sightRadius * Mathf.Cos(ang * Mathf.Deg2Rad);
+            
+
+        //        //if the targted tile is active
+        //        if (fogTiles[(int)pos.x, (int)pos.y].activeSelf != visibility)
+        //        {
+        //            //set to be inactive
+        //            fogTiles[x, y].SetActive(visibility);
+        //        }
+        //    }            
+        //}
     }
+ 
   
 }
---
+++ Last_Apocalypse/Assets/Behaviours/QuickWorld.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuickWorld : MonoBehaviour
+{
+    public Sprite floor;
+    public int mapWidth, mapHeight, tileWidth, tileHeight;
+
+    void Start()
+    {
+        GameObject world = new GameObject("World");
+
+        for (int x = 0; x < mapWidth; x++)
+        {
+            for (int y = 0; y < mapHeight; y++)
+            {
+                GameObject tile = new GameObject("tile " + x + " " + y);
+
+                tile.transform.position = new Vector3(x * (tileWidth / 100.0f), y * (tileHeight / 100.0f), 0.0f);
+
+                tile.AddComponent<SpriteRenderer>();
+                tile.GetComponent<SpriteRenderer>().sprite = floor;
+                tile.transform.parent = this.transform;                  
+                               
+            }
+        }
+
+    }
+
+}
