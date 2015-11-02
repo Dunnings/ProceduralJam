78763def7fce1af8ff22a8bbdef5a9179b5e3b8b
Dan Weston <westondaniel93@gmail.com>
2015-11-02 16:33:33 +0000

    I am ashamed of how gash this game over screen is </3

---
+++ Last_Apocalypse/Assets/Classes/RetryScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RetryScript : MonoBehaviour 
+{
+
+	// Use this for initialization
+
+    public void LoadLevel()
+    {
+        Application.LoadLevel("level1");
+    }
+}
