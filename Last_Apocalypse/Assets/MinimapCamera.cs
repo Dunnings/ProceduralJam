006f68d8031b389413a829e74338e217bafb8bdf
Dunnings <mrdaviddunnings@gmail.com>
2015-11-02 11:57:40 +0000

    Made UI a prefab

---
+++ Last_Apocalypse/Assets/MinimapCamera.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinimapCamera : MonoBehaviour {
+    
+    public Shader unlitShader;
+
+    void Start()
+    {
+        //unlitShader = Shader.Find("Unlit/Texture");
+        GetComponent<Camera>().SetReplacementShader(unlitShader, "");
+        gameObject.SetActive(false);
+    }
+}
