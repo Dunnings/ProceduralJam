99269c608082f0770a66d14bbadb6d592c766d1b
Sam Endean <samendean@hotmail.com>
2015-11-03 17:16:22 +0000

    UPDATED BUILDING PANELS
    INSTRUCTIONS:
    1 - PULL THE BORDER PREFAB INTO THE SCENE AT (0,0)
    2 - PULL ITEM LIST FROM SAM SCENE TO ANY OTHER SCENE

---
+++ Last_Apocalypse/Assets/Behaviours/RoofCollision.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoofCollision : MonoBehaviour {
+
+	public GameObject ParentBuilding;
+	public int roofDesignation;
+	
+	void Start () {
+	
+	}
+	
+
+	void Update ()
+	{	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		ParentBuilding.GetComponent<buildingRoof> ().collisions [roofDesignation] = true;
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		ParentBuilding.GetComponent<buildingRoof> ().collisions [roofDesignation] = false;
+	}
+}
---
+++ Last_Apocalypse/Assets/Behaviours/buildingRoof.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buildingRoof : MonoBehaviour {
+
+	public GameObject[] RoofQuads;
+	public bool[] collisions;
+
+	bool rendering = true;
+
+	void Start()
+	{
+		collisions = new bool[RoofQuads.Length];
+	}
+
+	void Update()
+	{
+		bool colliding = false;
+
+		for (int i = 0; i < (RoofQuads.Length); i++)
+		{
+			if (collisions[i])
+			{
+				colliding = true;
+				break;
+			}
+		}
+
+		if (colliding && rendering)
+		{
+			for (int i = 0; i < (RoofQuads.Length); i++)
+			{
+				RoofQuads[i].GetComponent<MeshRenderer> ().enabled = false;
+			}
+			rendering = false;
+		} 
+		else if (!colliding && !rendering)
+		{
+			for (int i = 0; i < (RoofQuads.Length); i++)
+			{
+				RoofQuads[i].GetComponent<MeshRenderer> ().enabled = true;
+			}
+			rendering = true;
+		}
+
+	}
+}
